4db284c6a974a422cb2249fd13bff354ad0ca27d
mjones129 <me@mattjones.tech>
2023-10-09 18:35:34 -0400

    check for empty inputs, handle textdomain

--- themegen.js
+++ themegen.js
@@ -1,13 +1,38 @@
 const prompt = require('prompt-sync') ();
 const fs = require('fs');
 
-const themeName = prompt("What's the name of your theme?");
+let themeTags = '';
+let themeVersion = '0.0.1';
+
+const themeName = prompt("What's the name of your theme? ");
 console.log("That's an awesome name. Good thinking!");
-const authorName = prompt("What's the author's name?");
+const authorName = prompt("What's the author's name? ");
 console.log(`Nice to meet you, ${authorName}!`);
-const authorURI = prompt(`Do you have a website, ${authorName}?`);
+const authorURI = prompt(`Do you have a website, ${authorName}? `);
 console.log("Awesome, I'll be sure to include that.");
-const themeDesc = prompt("How would you describe this theme?");
+const themeDesc = prompt("How would you describe this theme? ");
+console.log("Very nice!");
+const themeTagCheck = prompt("Would you like to add any tags to your theme? (y/n) ");
+
+if (themeTagCheck == "y") {
+    let themeTags = prompt('Please enter your theme tags: ');
+}
+
+
+const versionCheck = prompt("I assume this is version 0.0.1? (y/n) ");
+
+if (versionCheck == "y") {
+    let themeVersion = "0.0.1";
+} if (versionCheck == "n") {
+    let themeVersion = prompt("Please enter version number: ");
+}
+
+//remove spaces from theme name to create text domain
+const textDomainCondensed = themeName.split(" ").join("");
+//set the text domain to all lowercase
+const textDomain = textDomainCondensed.toLowerCase();
+
+console.log(`Congrats, ${authorName}! Your theme has been generated!`);
 
 let stylesheetData =   `/*
 Theme Name: ${themeName}
@@ -15,14 +40,14 @@ Theme URI: https://wordpress.org/themes/twentytwenty/
 Author: ${authorName}
 Author URI: ${authorURI}
 Description: ${themeDesc}
-Tags: blog, one-column, custom-background, custom-colors, custom-logo, custom-menu, editor-style, featured-images, footer-widgets, full-width-template, rtl-language-support, sticky-post, theme-options, threaded-comments, translation-ready, block-styles, wide-blocks, accessibility-ready
-Version: 1.3
-Requires at least: 5.0
-Tested up to: 5.4
-Requires PHP: 7.0
+Tags: ${themeTags}
+Version: ${themeVersion}
+Requires at least: 6.3.1
+Tested up to: 6.3.1
+Requires PHP: 8.2
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: twentytwenty
+Text Domain: ${textDomain}
 This theme, like WordPress, is licensed under the GPL.
 Use it to make something cool, have fun, and share what you've learned with others.
 */`
