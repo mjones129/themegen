cfe85d030646d49832c620ef7227841928f96355
mjones129 <me@mattjones.tech>
2023-11-01 11:43:16 -0400

    enqueue stylesheet

--- lib/functions.php
+++ lib/functions.php
@@ -1,5 +1,8 @@
 <?php
-//return current year
+//enqueue stylesheet
+wp_enqueue_style('themegenStyle', './style.css', [], null, 'all');
+
+//return current year to the footer
 add_shortcode('this_year', 'get_current_year');
 function get_current_year() {
     $year = date('Y');
