17eccadbe79159a7299f7d330e23fe996bfde70d
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-18 13:05:07 -0500

    start on header

---
+++ lib/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <?php wp_head(); ?>
+    </head>
+<body>
