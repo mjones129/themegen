8440a2e1e1fdf2069ad1ccc1dde101f52a1e0eba
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-21 16:50:07 -0500

    remove unused shortcode

--- lib/functions.php
+++ lib/functions.php
@@ -7,10 +7,3 @@ function themegen_styles() {
     wp_enqueue_script('bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', [], null, true);
 }
 // add_action('wp_enqueue_scripts', 'themegen_styles');
-
-//return current year to the footer
-function get_current_year() {
-    $year = date('Y');
-    return $year;
-}
-add_shortcode('this_year', 'get_current_year');
