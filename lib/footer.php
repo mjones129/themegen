4f94149446b34a5130072cda9d6cfdeebe22cf7c
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-21 16:48:09 -0500

    replace broken shortcode with simple echo

--- lib/footer.php
+++ lib/footer.php
@@ -1,4 +1,4 @@
-<p>Copyright [this_year]. </p>
+<p>Copyright <?php echo date('Y'); ?>. </p>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
