6a3b63796ccfada36fb65b2ca08ac02f2b8bbcb8
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-21 06:34:12 -0500

    enqueue scripts and styles with an actual hook

--- lib/functions.php
+++ lib/functions.php
@@ -1,10 +1,15 @@
 <?php
 //enqueue stylesheet
-wp_enqueue_style('themegenStyle', './style.css', [], null, 'all');
+function themegen_styles() {
+    wp_enqueue_style('themegenStyle', get_template_directory_uri() . '/style.css', [], null, 'all');
+    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', [], null, 'all');
+    wp_enqueue_script('bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', [], null, true);
+}
+// add_action('wp_enqueue_scripts', 'themegen_styles');
 
 //return current year to the footer
-add_shortcode('this_year', 'get_current_year');
 function get_current_year() {
     $year = date('Y');
     return $year;
 }
+add_shortcode('this_year', 'get_current_year');
