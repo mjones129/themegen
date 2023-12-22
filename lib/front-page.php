1c17b280ae94660bcc31c5284ff7aa6db4b307dc
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-22 09:19:39 -0500

    add random images to the cards

--- lib/front-page.php
+++ lib/front-page.php
@@ -11,7 +11,7 @@ get_header(); ?>
   <div class="row align-items-center">
     <div class="col">
     <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="https://source.unsplash.com/random" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -21,7 +21,7 @@ get_header(); ?>
     </div>
     <div class="col">
     <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="https://source.unsplash.com/random" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -31,7 +31,7 @@ get_header(); ?>
     </div>
     <div class="col">
     <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="https://source.unsplash.com/random" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
