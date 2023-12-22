cb158f83177813351fc812ae3313b9fe04303029
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-22 09:17:15 -0500

    add 3 cards to the homepage

--- lib/front-page.php
+++ lib/front-page.php
@@ -1,5 +1,47 @@
 <?php
 // wp_head();
-get_header();
-the_content();
-get_footer();
+get_header(); ?>
+
+<div class="container">
+    <?php the_content(); ?>
+</div>
+
+<!-- 3 cards in a container -->
+<div class="container mt-5 mb-5 text-center">
+  <div class="row align-items-center">
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+    <?php get_footer(); ?>
+</div>
