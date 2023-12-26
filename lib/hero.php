eba346bff57f644b0fa4063701978c74d8d217dd
Matt Jones <29353833+mjones129@users.noreply.github.com>
2023-12-26 16:19:52 -0500

    add hero module

---
+++ lib/hero.php
@@ -0,0 +1,8 @@
+<div class="d-flex flex-column vh-100 px-6 mb-5 justify-content-center"
+style="background-image: url('https://picsum.photos/1920/1080');">
+<div class="container">
+    <h1 class="text-left text-light">Some hero</h1>
+    <p class="text-light fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt possimus animi ullam vitae, ducimus ea. Culpa ad rem beatae, vitae voluptates, debitis porro illo, placeat quia quae aperiam. Ab, cupiditate!</p>
+    <button class="btn btn-primary">Some Button</button>
+</div>
+</div>
