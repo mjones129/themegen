a2bf72cbaf9c49cedcbd8ae99afacac45fdb3892
Matt Jones <me@mattjones.tech>
2023-12-31 17:24:56 -0500

    copy screenshot for blocks themes

--- themegen.js
+++ themegen.js
@@ -1,207 +1,226 @@
 //import the essentials
-import figlet from 'figlet';
-import {mkdir, copyFile, writeFile} from 'fs/promises';
-import promptSync from 'prompt-sync'; const prompt = promptSync();
-import {downloadPlugin} from './axiosRequest.js';
-
+import figlet from "figlet";
+import { mkdir, copyFile, writeFile } from "fs/promises";
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+import { downloadPlugin } from "./axiosRequest.js";
 
 //set some defaults
-let themeTags = '';
-let themeVersion = '0.0.1';
+let themeTags = "";
+let themeVersion = "0.0.1";
 const options = {
-    encoding: 'utf8',
-    mode: 0o755,
-    flag: ''
+  encoding: "utf8",
+  mode: 0o755,
+  flag: "",
 };
-const blocksIndex = './templates/index.html';
-const blocksIndexData = '';
-const classicIndex = './index.php';
-const classicIndexData = '';
+const blocksIndex = "./templates/index.html";
+const blocksIndexData = "";
+const classicIndex = "./index.php";
+const classicIndexData = "";
 let slugs = [];
-let bgColor = 'white';
-let fontColor = 'black';
-let primaryColor = '#6ab1eb';
-
+let bgColor = "white";
+let fontColor = "black";
+let primaryColor = "#6ab1eb";
 
 //begin program
 console.log(
-    figlet.textSync("ThemeGen", {
-        font: 'Cybermedium',
-        horizontalLayout: 'default',
-        verticalLayout: 'default',
-        width: 80,
-        whitespaceBreak: true,
-    })
+  figlet.textSync("ThemeGen", {
+    font: "Cybermedium",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  }),
 );
 
 console.log("Welcome to ThemGen!");
 const themeType = prompt("Is this a Blocks theme or a Classic theme? (b/c): ");
-if (themeType == 'b') {
-    try {
-        const tempDir = await mkdir('./templates');
-        await copyFile('./lib/index.html', './templates/index.html');
-        const partsDir = await mkdir('./parts');
-        await copyFile('./lib/header.html', './parts/header.html');
-        await copyFile('./lib/footer.html', './parts/footer.html');
-    } catch(err) {
-        console.error(err.message);
-    }
-    copyFile('./lib/theme.json', './theme.json');
+if (themeType == "b") {
+  try {
+    const tempDir = await mkdir("./templates");
+    await copyFile("./lib/index.html", "./templates/index.html");
+    const partsDir = await mkdir("./parts");
+    await copyFile("./lib/header.html", "./parts/header.html");
+    await copyFile("./lib/footer.html", "./parts/footer.html");
+  } catch (err) {
+    console.error(err.message);
+  }
+  copyFile("./lib/theme.json", "./theme.json");
+  copyFile("./lib/screenshot.png", "./screenshot.png");
 }
-if (themeType == 'c') {
-    try {
-        const includesDir = await mkdir('./includes');
-        await copyFile('./lib/accordion.php', './includes/accordion.php');
-        await copyFile('./lib/hero.php', './includes/hero.php');
-        await copyFile('./lib/cards.php', './includes/cards.php');
-    } catch(err) {
-        console.error(err.message);
-    }
-    copyFile('./lib/header.php', './header.php');
-    copyFile('./lib/footer.php', './footer.php');
-    copyFile('./lib/front-page.php', './front-page.php');
-    copyFile('./lib/functions.php', './functions.php');
-    copyFile('./lib/screenshot.png', './screenshot.png');
-    copyFile('./lib/index.php', './index.php');
+if (themeType == "c") {
+  try {
+    const includesDir = await mkdir("./includes");
+    await copyFile("./lib/accordion.php", "./includes/accordion.php");
+    await copyFile("./lib/hero.php", "./includes/hero.php");
+    await copyFile("./lib/cards.php", "./includes/cards.php");
+  } catch (err) {
+    console.error(err.message);
+  }
+  copyFile("./lib/header.php", "./header.php");
+  copyFile("./lib/footer.php", "./footer.php");
+  copyFile("./lib/front-page.php", "./front-page.php");
+  copyFile("./lib/functions.php", "./functions.php");
+  copyFile("./lib/screenshot.png", "./screenshot.png");
+  copyFile("./lib/index.php", "./index.php");
 }
 const themeName = prompt("What's the name of your theme? ");
 console.log("That's an awesome name. Good thinking!");
 const authorName = prompt("What's the author's name? ");
 console.log(`Nice to meet you, ${authorName}!`);
-const authorURI = prompt(`Have a website, ${authorName}? Please enter the URL: `);
+const authorURI = prompt(
+  `Have a website, ${authorName}? Please enter the URL: `,
+);
 console.log("Awesome, I'll be sure to include that.");
 const themeDesc = prompt("How would you describe this theme? ");
 console.log("Very nice!");
-const themeTagCheck = prompt("Would you like to add any tags to your theme? (y/n) ");
+const themeTagCheck = prompt(
+  "Would you like to add any tags to your theme? (y/n) ",
+);
 
 if (themeTagCheck == "y") {
-    themeTags = prompt('Please enter your theme tags: ');
+  themeTags = prompt("Please enter your theme tags: ");
 }
-
 
 const versionCheck = prompt("I assume this is version 0.0.1? (y/n) ");
 
 if (versionCheck == "y") {
-    themeVersion = "0.0.1";
-} if (versionCheck == "n") {
-    themeVersion = prompt("Please enter version number: ");
+  themeVersion = "0.0.1";
+}
+if (versionCheck == "n") {
+  themeVersion = prompt("Please enter version number: ");
 }
 
-const colors = prompt("Would you like to define custom colors for your theme? (y/n) ");
-if (colors === 'n') {
-    console.log('No custom colors set.');
+const colors = prompt(
+  "Would you like to define custom colors for your theme? (y/n) ",
+);
+if (colors === "n") {
+  console.log("No custom colors set.");
 }
-if (colors === 'y') {
-    bgColor = prompt("Please enter the hex code of the page background: ");
-    fontColor = prompt("Please enter the hex code of the font color: ");
-    primaryColor = prompt("Please enter the hex code of the header and footer: ");
-}
-
-const allOrNothing = prompt("Plugin time! Install the (u)sual suspects, (c)herry pick what you want, or (n)o plugins at all? (u/c/n) ");
-
-if (allOrNothing === 'c') {
-    const ecomm = prompt("Install WooCommerce? (y/n) ");
-    const wtf = prompt("Install What The File? (y/n) ");
-    const bsr = prompt("Install Better Search Replace? (y/n) ");
-    const emr = prompt("Install Enable Media Replace? (y/n) ");
-    const eio = prompt("Install EWWW Image Optimizer? (y/n) ");
-    const mwpw = prompt("Install ManageWP Worker? (y/n) ");
-    const redir = prompt("Install Redirection? (y/n) ");
-    const ssl = prompt("Install Really Simple SSL? (y/n) ");
-    const svgsupport = prompt("Install SVG Support? (y/n) ");
-    const wf = prompt("Install Wordfence Security? (y/n) ");
-    const wpfc = prompt("Install WP Fastest Cache? (y/n) ");
-    const hidelogin = prompt("Install WPS Hide Login? (y/n) ");
-    const yoast = prompt("Install Yoast SEO? (y/n) ");
-    const ydp = prompt("Install Yoast Duplicate Post? (y/n) ");
-
-    if (ecomm === 'y') {
-        slugs.push('woocommerce');
-    } else {
-        console.log('WooCommerce not downloaded.');
-    }
-    
-    if (wtf === 'y') {
-        slugs.push('what-the-file');
-    } else {
-        console.log('What the file not downloaded');
-    }
-    
-    if (bsr === 'y') {
-        slugs.push('better-search-replace');
-    } else {
-        console.log('Better Search Replace not downloaded.');
-    }
-    
-    if (eio === 'y') {
-        slugs.push('ewww-image-optimizer');
-    } else {
-        console.log('EWWW Image Optimizer not downloaded.');
-    }
-    
-    if (mwpw === 'y') {
-        slugs.push('worker');
-    } else {
-        console.log('ManageWP Worker not downloaded.');
-    }
-    
-    if (wf === 'y') {
-        slugs.push('wordfence');
-    } else {
-        console.log('Wordfence Security not downloaded.');
-    }
-    
-    if (wpfc === 'y') {
-        slugs.push('wp-fastest-cache');
-    } else {
-        console.log('WP Fastest Cace not downloaded.');
-    }
-    if (hidelogin === 'y') {
-        slugs.push('wps-hide-login');
-    } else {
-        console.log("WPS Hide Login not downloaded.");
-    }
-    if (yoast === 'y') {
-        slugs.push('wordpress-seo');
-    } else {
-        console.log("Yoast SEO not downloaded.");
-    }
-    if (ssl === 'y') {
-        slugs.push('really-simple-ssl');
-    } else {
-        console.log('Really Simple SSL not downloaded.');
-    }
-    
-    if (emr === 'y') {
-        slugs.push('enable-media-replace');
-    } else {
-        console.log("Enalbe Media Replace not downloaded.");
-    }
-
-    if (redir === 'y') {
-        slugs.push('redirection');
-    } else {
-        console.log('Redirection not downloaded.');
-    }
-
-    if (ydp === 'y') {
-        slugs.push('duplicate-post');
-    } else {
-        console.log('Yoast Duplicate Post not downloaded.');
-    }
-    if (svgsupport === 'y') {
-        slugs.push('svg-support');
-    } else {
-        console.log('SVG Support plugin not downloaded.');
-    }
+if (colors === "y") {
+  bgColor = prompt("Please enter the hex code of the page background: ");
+  fontColor = prompt("Please enter the hex code of the font color: ");
+  primaryColor = prompt("Please enter the hex code of the header and footer: ");
 }
 
-if (allOrNothing === 'u') {
-    slugs.push('what-the-file', 'better-search-replace', 'ewww-image-optimizer', 'svg-support', 'enable-media-replace', 'really-simple-ssl', 'wordfence', 'wp-fastest-cache', 'wps-hide-login', 'wordpress-seo');
+const allOrNothing = prompt(
+  "Plugin time! Install the (u)sual suspects, (c)herry pick what you want, or (n)o plugins at all? (u/c/n) ",
+);
+
+if (allOrNothing === "c") {
+  const ecomm = prompt("Install WooCommerce? (y/n) ");
+  const wtf = prompt("Install What The File? (y/n) ");
+  const bsr = prompt("Install Better Search Replace? (y/n) ");
+  const emr = prompt("Install Enable Media Replace? (y/n) ");
+  const eio = prompt("Install EWWW Image Optimizer? (y/n) ");
+  const mwpw = prompt("Install ManageWP Worker? (y/n) ");
+  const redir = prompt("Install Redirection? (y/n) ");
+  const ssl = prompt("Install Really Simple SSL? (y/n) ");
+  const svgsupport = prompt("Install SVG Support? (y/n) ");
+  const wf = prompt("Install Wordfence Security? (y/n) ");
+  const wpfc = prompt("Install WP Fastest Cache? (y/n) ");
+  const hidelogin = prompt("Install WPS Hide Login? (y/n) ");
+  const yoast = prompt("Install Yoast SEO? (y/n) ");
+  const ydp = prompt("Install Yoast Duplicate Post? (y/n) ");
+
+  if (ecomm === "y") {
+    slugs.push("woocommerce");
+  } else {
+    console.log("WooCommerce not downloaded.");
+  }
+
+  if (wtf === "y") {
+    slugs.push("what-the-file");
+  } else {
+    console.log("What the file not downloaded");
+  }
+
+  if (bsr === "y") {
+    slugs.push("better-search-replace");
+  } else {
+    console.log("Better Search Replace not downloaded.");
+  }
+
+  if (eio === "y") {
+    slugs.push("ewww-image-optimizer");
+  } else {
+    console.log("EWWW Image Optimizer not downloaded.");
+  }
+
+  if (mwpw === "y") {
+    slugs.push("worker");
+  } else {
+    console.log("ManageWP Worker not downloaded.");
+  }
+
+  if (wf === "y") {
+    slugs.push("wordfence");
+  } else {
+    console.log("Wordfence Security not downloaded.");
+  }
+
+  if (wpfc === "y") {
+    slugs.push("wp-fastest-cache");
+  } else {
+    console.log("WP Fastest Cace not downloaded.");
+  }
+  if (hidelogin === "y") {
+    slugs.push("wps-hide-login");
+  } else {
+    console.log("WPS Hide Login not downloaded.");
+  }
+  if (yoast === "y") {
+    slugs.push("wordpress-seo");
+  } else {
+    console.log("Yoast SEO not downloaded.");
+  }
+  if (ssl === "y") {
+    slugs.push("really-simple-ssl");
+  } else {
+    console.log("Really Simple SSL not downloaded.");
+  }
+
+  if (emr === "y") {
+    slugs.push("enable-media-replace");
+  } else {
+    console.log("Enalbe Media Replace not downloaded.");
+  }
+
+  if (redir === "y") {
+    slugs.push("redirection");
+  } else {
+    console.log("Redirection not downloaded.");
+  }
+
+  if (ydp === "y") {
+    slugs.push("duplicate-post");
+  } else {
+    console.log("Yoast Duplicate Post not downloaded.");
+  }
+  if (svgsupport === "y") {
+    slugs.push("svg-support");
+  } else {
+    console.log("SVG Support plugin not downloaded.");
+  }
 }
 
-if (allOrNothing === 'n') {
-    console.log('Setup complete. Thanks for using ThemeGen!');
+if (allOrNothing === "u") {
+  slugs.push(
+    "what-the-file",
+    "better-search-replace",
+    "ewww-image-optimizer",
+    "svg-support",
+    "enable-media-replace",
+    "really-simple-ssl",
+    "wordfence",
+    "wp-fastest-cache",
+    "wps-hide-login",
+    "wordpress-seo",
+  );
+}
+
+if (allOrNothing === "n") {
+  console.log("Setup complete. Thanks for using ThemeGen!");
 }
 
 //remove spaces from theme name to create text domain
@@ -210,7 +229,7 @@ const textDomainCondensed = themeName.split(" ").join("");
 const textDomain = textDomainCondensed.toLowerCase();
 
 //now that we have all the answers, we can populate the main style.css
-let stylesheetData =   `/*
+let stylesheetData = `/*
 Theme Name: ${themeName}
 Theme URI: https://wordpress.org/themes/twentytwenty/
 Author: ${authorName}
@@ -246,18 +265,15 @@ header, footer {
     background-color: var(--themegen-pri);
 }
 
-`
+`;
 
-
-await writeFile('style.css', stylesheetData, (err) => {
-    if(err) throw err;
-    console.log(`Stylesheet generated!`);
+await writeFile("style.css", stylesheetData, (err) => {
+  if (err) throw err;
+  console.log(`Stylesheet generated!`);
 });
 
-
-
 if (slugs.length !== 0) {
-    for (let i = 0; slugs[i]; i++) {
-        await downloadPlugin(slugs[i]);
-    }
+  for (let i = 0; slugs[i]; i++) {
+    await downloadPlugin(slugs[i]);
+  }
 }
