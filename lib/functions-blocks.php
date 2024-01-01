a022311421b4a6004fd470a4b2d804d80092dbe8
Matt Jones <me@mattjones.tech>
2024-01-01 09:31:00 -0500

    update year shortcode for block themes

--- lib/functions-blocks.php
+++ lib/functions-blocks.php
@@ -1,5 +1,12 @@
 <?php
+//Add stylesheet
 function add_style() {
   wp_enqueue_style('blocks-styles', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'add_style');
+
+//add year shortcode
+function tg_year() {
+  return date("Y");
+}
+add_shortcode('year', 'tg_year');
